ec83530cfe6f9d0049cd9bae2a31529267502f34
Devesh <deveshpillai100@gmail.com>
2023-02-27 09:52:11 +0530

    RECURSION---432 int four three two

---
+++ program2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+void turn(int n, string arr[]){
+    if(n==0)
+        return;
+        
+    int digit=n%10;
+    n=n/10;
+    
+    turn(n,arr);
+    
+    cout<<arr[digit]<<" ";
+}
+
+int main() {
+    string arr[10]={"zero","one","two","three","four","five"};
+    int n;
+    cin>>n;
+    turn(n,arr);
+
+    return 0;
+}
