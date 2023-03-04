2d9baea6ee4a76c6e2c8527625addd2b90bfb8ca
Devesh <deveshpillai100@gmail.com>
2023-03-05 01:55:35 +0530

    StringPermutationsRECURSION

---
+++ StringPermutationsRECURSION.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution{
+private:
+
+    void solve(vector<int> nums,vector<vector<int> >& ans,int index){
+        if(index>= nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i=index; i<nums.size(); i++){
+            swap(nums[index],nums[i]);
+            solve(nums,ans,index+1);
+            //backtrack
+
+            swap(nums[index],nums[i]);
+        }
+    }
+public:
+
+    vector<vector<int>> permute(vector<int>& nums){
+        vector<vector<int>> ans;
+        int index=0;
+        solve(nums,ans,index);
+        return ans;
+    }
+};
